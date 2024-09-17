3b8bc793163be3638f319edb4958d700d1f2b2bb
rijogeorge <rijothedeveloper@gmail.com>
2024-09-17 08:24:00 -0700

    first commit

---
+++ backend/findinmymh/src/main/java/com/findinmymh/FindinmymhApplication.java
@@ -0,0 +1,13 @@
+package com.findinmymh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindinmymhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindinmymhApplication.class, args);
+	}
+
+}
